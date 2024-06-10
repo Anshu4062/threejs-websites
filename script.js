49ed6159a5b0e3f933e4695b422b3c0222fde30a
Suryansh Singh <supremesofficialyt@gmail.com>
2024-06-10 13:29:27 +0530

    Files Updated

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-import * as THREE from three;
+const canvas = document.querySelector(".webgl");
 
-console.log("Hi there!");
+//Scene
+const scene = new THREE.Scene();
+
+//Red Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "#ff0000" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//Sizes
+const sizes = {
+    width: 800,
+    height: 600
+};
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height);
+scene.add(camera);
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
