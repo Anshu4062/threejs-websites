c7375690b1a93c5a547dc79e2fd86bd380bbb57d
Suryansh Singh <supremesofficialyt@gmail.com>
2024-06-18 00:05:53 +0530

    Files Updated

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -1,27 +1,41 @@
 import './style.css'
 import * as THREE from 'three'
 
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
+
 // Scene
 const scene = new THREE.Scene()
+
 
 /**
  * Objects
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: "#ff0000" })
+const material = new THREE.MeshBasicMaterial({ color: "#5A639C" })
 const mesh = new THREE.Mesh(geometry, material)
 // mesh.position.x = 0.7
 // mesh.position.y = -0.6
 // mesh.position.z = 1
 
 mesh.position.set(0.7, -0.6, 1)  // Does same as -> mesh.position.x = 0.7  mesh.position.y = -0.6  mesh.position.z = 1
-
 // console.log(mesh.position.length())  // Gives the distance of mesh from center of scene
 
+//Scale of mesh
+// mesh.scale.x = 1  // Increases the size of x-axis of mesh
+// mesh.scale.y = 1
+// mesh.scale.z = 1
+mesh.scale.set(2, 0.5, 0.5)
+
 scene.add(mesh)
+
+
+//Axes Helper
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
+
 
 /**
  * Sizes
@@ -31,6 +45,7 @@ const sizes = {
     height: 600
 }
 
+
 /**
  * Camera
  */
@@ -39,6 +54,7 @@ camera.position.z = 3
 scene.add(camera)
 
 // console.log(mesh.position.distanceTo(camera.position))  // For checking distance between camera and mesh
+
 
 /**
  * Renderer
