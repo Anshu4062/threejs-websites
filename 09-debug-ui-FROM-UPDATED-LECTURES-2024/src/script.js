073fe36e201a8d3eea89e91b747a2d8f86be51e8
Suryansh Singh <supremesofficialyt@gmail.com>
2024-07-31 03:30:15 +0530

    Files Updated

--- 09-debug-ui-FROM-UPDATED-LECTURES-2024/src/script.js
+++ 09-debug-ui-FROM-UPDATED-LECTURES-2024/src/script.js
@@ -1,6 +1,6 @@
-import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from 'gsap'
 
 /**
  * Base
@@ -11,24 +11,17 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-
-const geometry = new THREE.BufferGeometry()
-const count = 500
-const array = new Float32Array( count * 3 * 3 )
-
-for (let i = 0; i < (count * 3 * 3); i++) {
-    array[i] = (Math.random() - 0.5) * 3
-}
-
-const bufferAttribute = new THREE.BufferAttribute(array, 3)  //3 is the value each vertex will take
-geometry.setAttribute("position", bufferAttribute)
-
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-// Sizes
+/**
+ * Sizes
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -49,23 +42,32 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-// Camera
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
 const tick = () =>
